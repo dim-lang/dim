6a14d403de388140a15caa845db6149124e8a5bf
linrongbin <linrongbin16@gmail.com>
2019-06-18 21:21:50 +0800

    update build

--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
 @echo off
 
-set DEBUG=windows/debug
-RELEASE=windows/release
+set DEBUG=debug
+set RELEASE=release
+
+if not exist %DEBUG% md %DEBUG%
+if not exist %RELEASE% md %RELEASE%
+
+cd %DEBUG% && cmake -DCMAKE_BUILD_TYPE=Debug ..\src && cd ..
+cd %RELEASE% && cmake -DCMAKE_BUILD_TYPE=Release ..\src && cd ..
