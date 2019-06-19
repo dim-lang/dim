8e4e4a90e468348f29affd45cd93e64b97630e44
linrongbin <rolin@ebay.com>
2019-06-19 10:05:13 +0800

    update build

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @echo off
 
+@REM prepare build folder
 set DEBUG=debug
 set RELEASE=release
-
 if not exist %DEBUG% md %DEBUG%
 if not exist %RELEASE% md %RELEASE%
 
+@REM build
 cd %DEBUG% && cmake --config Debug ..\src && cd ..
 cd %RELEASE% && cmake --config Release ..\src && cd ..
