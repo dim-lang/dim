f3c64c144389115441b78b83c9ddeb24922b19ba
linrongbin <linrongbin16@gmail.com>
2019-06-20 06:42:11 +0800

    update cmake

---
+++ src/CMakeUNIX.cmake
@@ -0,0 +1,23 @@
+# Copyright 2019- <fastype.org>
+# Apache License Version 2.0
+
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+
+find_package(Curses REQUIRED)
+message(CURSES_INCLUDE_DIR: ${CURSES_INCLUDE_DIR})
+message(CURSES_LIBRARIES: ${CURSES_LIBRARIES})
+message(CURSES_LIBRARY_DIR: ${CURSES_LIBRARY_DIR})
+
+set(F_INCLUDE_DIR
+    ${CURSES_INCLUDE_DIR}
+    )
+
+set(F_LIBRARIES
+    ${CURSES_LIBRARIES}
+    )
+
+set(F_LIBRARY_DIR
+    ${UNIX_LIBRARY_DIR}
+    )
---
+++ src/CMakeWindows.cmake
@@ -0,0 +1,13 @@
+# Copyright 2019- <fastype.org>
+# Apache License Version 2.0
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++14")
+
+set(F_INCLUDE_DIR 
+    )
+
+set(F_LIBRARIES
+    )
+
+set(F_LIBRARY_DIR
+    )
