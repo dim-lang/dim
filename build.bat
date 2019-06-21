1b87b625b9d91c5493c582ef030704299d812e4d
linrongbin <rolin@ebay.com>
2019-06-21 11:14:30 +0800

    update build

--- build.bat
+++ build.bat
@@ -1,19 +1,24 @@
 @echo off
 
-@REM create boost header
+@REM git submodules
+set ROOT=%cd%
+git submodule update --init
+cd src\boost && git submodule update --init && cd %ROOT%
+
+@REM third party library
 if not exist src\boost\boost (
-    cd src\boost && .\bootstrap.bat && .\b2.bat headers && cd ..\..
+    cd src\boost && .\bootstrap.bat && .\b2.bat headers && cd %ROOT%
 )
 if not exist -d src\icu\build (
-    md src\icu\build && cd src\icu\build && ..\icu4c\source\runConfigureICU Windows && make -j6 check && cd ..\..\..
+    md src\icu\build && cd src\icu\build && ..\icu4c\source\runConfigureICU Windows && cd %ROOT%
 )
 
-@REM prepare build folder
+@REM build folder
 set DEBUG=debug
 set RELEASE=release
 if not exist %DEBUG% md %DEBUG%
 if not exist %RELEASE% md %RELEASE%
 
 @REM build
-cd %DEBUG% && cmake --config Debug ..\src && cd ..
-cd %RELEASE% && cmake --config Release ..\src && cd ..
+cd %DEBUG% && cmake --config Debug ..\src && cd %ROOT%
+cd %RELEASE% && cmake --config Release ..\src && cd %ROOT%
