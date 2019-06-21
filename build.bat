f49547174ba5326733d053a21a73b0943eaf9a04
linrongbin <rolin@ebay.com>
2019-06-21 10:22:31 +0800

    add icu build

--- build.bat
+++ build.bat
@@ -4,6 +4,9 @@
 if not exist src\boost\boost (
     cd src\boost && .\bootstrap.bat && .\b2.bat headers && cd ..\..
 )
+if not exist -d src\icu\build (
+    md src\icu\build && cd src\icu\build && ..\icu4c\source\runConfigureICU Windows && make -j6 check && cd ..\..\..
+)
 
 @REM prepare build folder
 set DEBUG=debug
