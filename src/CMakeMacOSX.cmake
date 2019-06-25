297203d7d11bd711648eb8ee63d4edeecfa4b310
linrongbin <rolin@ebay.com>
2019-06-25 14:34:59 +0800

    use git submodule instead of package manager

--- src/CMakeMacOSX.cmake
+++ src/CMakeMacOSX.cmake
@@ -6,7 +6,8 @@ set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 
-find_package(fmt REQUIRED)
+find_package(ICU COMPONENTS uc i18n REQUIRED)
+#find_package(fmt REQUIRED)
 find_package(Curses REQUIRED)
 find_package(Threads REQUIRED)
 set(Boost_USE_STATIC_LIBS OFF)
@@ -25,18 +26,24 @@ message(Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS})
 message(FMT_LIBRARIES: ${FMT_LIBRARIES})
 message(FMT_INCLUDE_DIRS: ${FMT_INCLUDE_DIRS})
 message(FMT_LIBRARY_DIRS: ${FMT_LIBRARY_DIRS})
+message(ICU4C_INCLUDE_DIRS: ${ICU4C_INCLUDE_DIRS})
 
 set(F_INC
     .
     ${CURSES_INCLUDE_DIRS}
-    fmt::fmt
     ${Boost_INCLUDE_DIRS}
+    Threads::Threads
+    fmt::fmt
+    ICU::uc
+    ICU::i18n
     )
 set(F_LIB
     ${CURSES_LIBRARIES}
-    Threads::Threads
     Boost::program_options
+    Threads::Threads
     fmt::fmt
+    ICU::uc
+    ICU::i18n
     )
 set(F_LIB_DIR
     ${CURSES_LIBRARY_DIRS}
