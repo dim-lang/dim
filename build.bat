b962ba3b15b6889da756f9f394700357eb7856ef
linrongbin <rolin@ebay.com>
2019-06-25 15:13:25 +0800

    spdlog v1.3.1

--- build.bat
+++ build.bat
@@ -7,32 +7,34 @@ set OS="Windows"
 echo [fastype] Build for %OS%
 
 @rem third party library
+cd src
 if not exist spdlog (
     echo [fastype] prepare *spdlog* v1.3.1
     git clone https://github.com/gabime/spdlog.git
-    cd spdlog && git checkout v1.3.1 && cd %ROOT%
+    cd spdlog && git checkout v1.3.1 && cd %ROOT%\src
     echo [fastype] prepare *spdlog* v1.3.1 - done
 )
 if not exist fmt (
     echo [fastype] prepare *fmt* v5.3.0
     git clone https://github.com/fmtlib/fmt.git
-    cd fmt && git checkout v5.3.0 && cd %ROOT%
+    cd fmt && git checkout v5.3.0 && cd %ROOT%\src
     echo [fastype] prepare *fmt* v5.3.0 - done
 )
 if not exist icu (
     echo [fastype] prepare *icu4c* release-64-2
     git clone https://github.com/unicode-org/icu.git
-    cd icu && git checkout release-64-2 && cd %ROOT%
+    cd icu && git checkout release-64-2 && cd %ROOT%\src
     echo [fastype] build *icu4c* release-64-2 manually: https://htmlpreview.github.io/?https://github.com/unicode-org/icu/blob/release-64-2/icu4c/readme.html#HowToBuildWindows
 )
 if not exist boost (
     echo [fastype] prepare *boost* boost-1.70.0
     git clone https://github.com/boostorg/boost.git
-    cd boost && git checkout boost-1.70.0 && git submodule update --init && .\bootstrap.bat && .\b2 && cd %ROOT%
+    cd boost && git checkout boost-1.70.0 && git submodule update --init && .\bootstrap.bat && .\b2 && cd %ROOT%\src
     echo [fastype] prepare *boost* boost-1.70.0 - done
 )
 
 @rem build
+cd %ROOT%
 set DEBUG=debug
 set RELEASE=release
 if not exist %DEBUG% md %DEBUG%
