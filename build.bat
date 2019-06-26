66ee7d87b3e5d852918f24f082eb84ab9e30e80e
linrongbin <rolin@ebay.com>
2019-06-26 10:25:43 +0800

    update build

--- build.bat
+++ build.bat
@@ -7,13 +7,14 @@ set OS="Windows"
 echo [fastype] Build for %OS%
 
 @rem third party library
-git submodule update --init
-echo [fastype] prepare *boost* boost-1.70.0
-cd %ROOT%\src\boost && git submodule update --init && .\bootstrap.bat && .\b2 && cd %ROOT%
-echo [fastype] prepare *boost* boost-1.70.0 - done
-echo [fastype] prepare *icu4c* release-64-2
+git submodule update --init --recursive
+echo [fastype] prepare spdlog v1.3.1 - done
+echo [fastype] prepare boost boost-1.70.0
+cd %ROOT%\src\boost .\bootstrap.bat && .\b2 && cd %ROOT%
+echo [fastype] prepare boost boost-1.70.0 - done
+echo [fastype] prepare icu4c release-64-2
 cd %ROOT%\src\icu && md build && cd build && ..\icu4c\source\runConfigureICU %OS% && make -j8 && cd %ROOT%
-echo [fastype] prepare *icu4c* release-64-2
+echo [fastype] prepare icu4c release-64-2 - done
 
 @rem build
 cd %ROOT%
