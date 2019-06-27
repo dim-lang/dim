4ced887a94b0390fb4d21ce59033cffe80b12a38
linrongbin <rolin@ebay.com>
2019-06-27 09:54:48 +0800

    update read file

--- src/NCursesTerm.cpp
+++ src/NCursesTerm.cpp
@@ -25,28 +25,45 @@ NCursesTerm::~NCursesTerm() { F_DEBUG(log, "Destruct"); }
 void NCursesTerm::show(const std::string &fileName) {
   F_DEBUGF(log, "fileName: {}", fileName);
 
-  int row, col, y, x;
+  int ch, prev = EOF, row, col;
+  int y, x;
+  FILE *fp = std::fopen(fileName.data(), "r");
 
   initscr();
   getmaxyx(stdscr, row, col);
-  F_DEBUGF(log, "row:{}, col:{}, LINES:{}", row, col, LINES);
-  y = 0;
-  x = 0;
-  mvprintw(y, x, "(%d,%d)", y, x);
-  y = row;
-  x = 0;
-  mvprintw(y - 1, x, "(%d-1,%d)", y, x);
-  y = 0;
-  x = col;
-  mvprintw(y, x - 10, "(%d,%d-10)", y, x);
-  y = row / 2;
-  x = col / 2;
-  mvprintw(y, x, "(%d,%d) (row:%d,col:%d)", y, x, row, col);
-  y = row;
-  x = col;
-  mvprintw(y - 1, x - 10, "(%d-1,%d-10)", y, x);
+  F_DEBUGF(log, "start loop, fp:{}", (void *)fp);
+  while ((ch = fgetc(fp)) != EOF) {
+    getyx(stdscr, y, x);
+    F_DEBUGF(log, "loop, ch:{} prev:{} y:{} x:{} row:{} col:{}", ch, prev, y, x,
+             row, col);
+    if (y == (row - 1)) {
+      F_DEBUG(log, "loop - 1");
+      printw("<-Press Any Key->");
+      getch();
+      clear();
+      move(0, 0);
+    }
+    if (prev == '/' && ch == '*') {
+      F_DEBUG(log, "loop - 2");
+      attron(A_BOLD);
+      getyx(stdscr, y, x);
+      move(y, x - 1);
+      printw("%c%c", '/', ch);
+    } else {
+      F_DEBUG(log, "loop - 3");
+      printw("%c", ch);
+    }
+    refresh();
+    if (prev == '*' && ch == '/') {
+      F_DEBUG(log, "loop - 4");
+      attroff(A_BOLD);
+    }
+    prev = ch;
+  }
+  F_DEBUGF(log, "end loop, fp:{}", (void *)fp);
   getch();
   endwin();
+  fclose(fp);
 }
 
 } // namespace fastype
---
+++ test/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  /**
+   * This is a comment
+   */
+  printf("Hello Fastype");
+  return 0;
+}
