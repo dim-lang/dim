c3046aea7d6613c6c8c1590c35b284a7bdb0c196
linrongbin <rolin@ebay.com>
2019-09-12 10:27:32 +0800

    save

---
+++ test/BlockTest.cpp
@@ -0,0 +1,19 @@
+// Copyright 2019- <fastype.org>
+// Apache License Version 2.0
+
+#include "Block.h"
+#include "catch2/catch.hpp"
+#include <cstring>
+
+#define HELLO_WORLD "hello world"
+#define GOODBYE_WORLD "goodbye world"
+
+TEST_CASE("Block", "[Block]") {
+
+  SECTION("Constructor") {
+    fastype::Block b;
+    REQUIRE(b.capacity() == 0);
+    REQUIRE(b.empty());
+    REQUIRE(b.size() == 0);
+  }
+}
--- test/RowTest.cpp
+++ test/RowTest.cpp
@@ -12,22 +12,22 @@
 TEST_CASE("Row", "[Row]") {
 
   SECTION("Args Constructor") {
-    fastype::Cowstr cs(100);
-    cs.concat(HELLO_WORLD, std::strlen(HELLO_WORLD));
+    // fastype::Cowstr cs(100);
+    // cs.concat(HELLO_WORLD, std::strlen(HELLO_WORLD));
 
-    fastype::Row r1(cs, 3, true);
-    REQUIRE((void *)r1.str().head() != nullptr);
-    REQUIRE(r1.str().size() == std::strlen(HELLO_WORLD));
-    REQUIRE(r1.str().capacity() >= 100);
-    REQUIRE(!r1.dirty());
-    REQUIRE(r1.lineNumber() == 3);
+    // fastype::Row r1(cs, 3, true);
+    // REQUIRE((void *)r1.str().head() != nullptr);
+    // REQUIRE(r1.str().size() == std::strlen(HELLO_WORLD));
+    // REQUIRE(r1.str().capacity() >= 100);
+    // REQUIRE(!r1.dirty());
+    // REQUIRE(r1.lineNumber() == 3);
 
-    cs.concat(GOODBYE_WORLD, std::strlen(GOODBYE_WORLD));
-    fastype::Row r2(cs, 17, false);
-    REQUIRE((void *)r2.str().head() != nullptr);
-    REQUIRE(r2.str().size() == std::strlen(GOODBYE_WORLD));
-    REQUIRE(r2.str().capacity() >= 0);
-    REQUIRE(r2.dirty());
-    REQUIRE(r2.lineNumber() == 17);
+    // cs.concat(GOODBYE_WORLD, std::strlen(GOODBYE_WORLD));
+    // fastype::Row r2(cs, 17, false);
+    // REQUIRE((void *)r2.str().head() != nullptr);
+    // REQUIRE(r2.str().size() == std::strlen(GOODBYE_WORLD));
+    // REQUIRE(r2.str().capacity() >= 0);
+    // REQUIRE(r2.dirty());
+    // REQUIRE(r2.lineNumber() == 17);
   }
 }
