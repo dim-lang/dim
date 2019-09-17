1f6ba5a745ae7be32392f1fe07636c3617086a34
linrongbin <rolin@ebay.com>
2019-09-17 18:51:21 +0800

    save

--- src/Random.h
+++ src/Random.h
@@ -3,6 +3,7 @@
 
 #pragma once
 #include "Logging.h"
+#include <boost/preprocessor/cat.hpp>
 #include <cmath>
 #include <cstdint>
 #include <limits>
@@ -13,16 +14,31 @@ namespace fastype {
 
 class Random {
 public:
-  // @return   [0, limit)
-  template <typename T> static T next(T limit = std::numeric_limits<T>::max()) {
-    F_CHECKF(limit > 0, "limit {} > 0", limit);
-    return next<T>(0, limit);
-  }
+#define F_DECL_RAND(Type, Name)                                                \
+  static Type BOOST_PP_CAT(next, Name)(Type right =                            \
+                                           std::numeric_limits<Type>::max());  \
+  static Type BOOST_PP_CAT(next, Name)(Type left, Type right)
 
-  // @return   [start, limit)
-  template <typename T> static T next(T start, T limit) {
-    return (llong_(engine_) % (limit - start)) + start;
-  }
+  // @return   [0, right)
+  // @return   [left, right)
+  F_DECL_RAND(short, Shrt);
+  F_DECL_RAND(unsigned short, UShrt);
+  F_DECL_RAND(int, Int);
+  F_DECL_RAND(unsigned int, UInt);
+  F_DECL_RAND(long, Long);
+  F_DECL_RAND(unsigned long, ULong);
+  F_DECL_RAND(long long, LLong);
+  F_DECL_RAND(unsigned long long, ULLong);
+  F_DECL_RAND(int8_t, Int8);
+  F_DECL_RAND(uint8_t, UInt8);
+  F_DECL_RAND(int16_t, Int16);
+  F_DECL_RAND(uint16_t, UInt16);
+  F_DECL_RAND(int32_t, Int32);
+  F_DECL_RAND(uint32_t, UInt32);
+  F_DECL_RAND(int64_t, Int64);
+  F_DECL_RAND(uint64_t, UInt64);
+
+#undef F_DECL_RAND
 
   static std::string nextAlpha(int limit);
 
@@ -37,7 +53,12 @@ public:
 private:
   static std::random_device device_;
   static std::mt19937 engine_;
+  static std::uniform_int_distribution<long> long_;
+  static std::uniform_int_distribution<unsigned long> ulong_;
   static std::uniform_int_distribution<long long> llong_;
+  static std::uniform_int_distribution<unsigned long long> ullong_;
+  static std::uniform_real_distribution<float> float_;
+  static std::uniform_real_distribution<double> double_;
 };
 
 } // namespace fastype
--- test/RandomTest.cpp
+++ test/RandomTest.cpp
@@ -2,6 +2,7 @@
 // Apache License Version 2.0
 
 #include "Random.h"
+#include "Logging.h"
 #include "catch2/catch.hpp"
 #include <algorithm>
 #include <cstddef>
@@ -12,9 +13,11 @@
 #define assertValid(i, IntType)                                                \
   do {                                                                         \
     IntType a = fastype::Random::next<IntType>();                              \
-    IntType b = fastype::Random::next<IntType>(i + 1);                         \
+    IntType b = fastype::Random::next<IntType>(                                \
+        std::min<IntType>(i + 1, std::numeric_limits<IntType>::max()));        \
     IntType c =                                                                \
         fastype::Random::next<IntType>(std::min(a, b), std::max(a, b));        \
+    F_INFOF("a:{} b:{} c:{}", a, b, c);                                        \
     REQUIRE(a >= 0);                                                           \
     REQUIRE(a < std::numeric_limits<IntType>::max());                          \
     REQUIRE(b >= 0);                                                           \
@@ -63,6 +66,62 @@ TEST_CASE("Random", "[Random]") {
     INFO("unsigned long long");
     for (int i = 0; i < TEST_MAX; i++) {
       assertValid(i, unsigned long long);
+    }
+  }
+
+  SECTION("int8_t") {
+    INFO("int8_t");
+    for (int i = 0; i < TEST_MAX; i++) {
+      assertValid(i, int8_t);
+    }
+  }
+
+  SECTION("uint8_t") {
+    INFO("uint8_t");
+    for (int i = 0; i < TEST_MAX; i++) {
+      assertValid(i, uint8_t);
+    }
+  }
+
+  SECTION("int16_t") {
+    INFO("int16_t");
+    for (int i = 0; i < TEST_MAX; i++) {
+      assertValid(i, int16_t);
+    }
+  }
+
+  SECTION("uint16_t") {
+    INFO("uint16_t");
+    for (int i = 0; i < TEST_MAX; i++) {
+      assertValid(i, uint16_t);
+    }
+  }
+
+  SECTION("int32_t") {
+    INFO("int32_t");
+    for (int i = 0; i < TEST_MAX; i++) {
+      assertValid(i, int32_t);
+    }
+  }
+
+  SECTION("uint32_t") {
+    INFO("uint32_t");
+    for (int i = 0; i < TEST_MAX; i++) {
+      assertValid(i, uint32_t);
+    }
+  }
+
+  SECTION("int64_t") {
+    INFO("int64_t");
+    for (int i = 0; i < TEST_MAX; i++) {
+      assertValid(i, int64_t);
+    }
+  }
+
+  SECTION("uint64_t") {
+    INFO("uint64_t");
+    for (int i = 0; i < TEST_MAX; i++) {
+      assertValid(i, uint64_t);
     }
   }
 }
