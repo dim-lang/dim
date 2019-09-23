9d1446772b6fbf39071bb65011538f802201a5e7
linrongbin <rolin@ebay.com>
2019-09-23 10:04:12 +0800

    update build

--- build_clean.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-# Copyright 2019- <fastype.org>
-# Apache License Version 2.0
-
-rm *.log
-cd debug && make clean && cd ..
-cd release && make clean && cd ..
--- build_test.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-# Copyright 2019- <fastype.org>
-# Apache License Version 2.0
-
-debug/test/fastype-test
