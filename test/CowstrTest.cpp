4576d6276166001c9b4b5571bebdc4e1efbcf38e
linrongbin <rolin@ebay.com>
2019-10-10 00:52:12 +0800

    optimize replacement

--- test/CowstrTest.cpp
+++ test/CowstrTest.cpp
@@ -10,6 +10,7 @@
 #include <cstring>
 #include <sstream>
 #include <string>
+#include <tuple>
 #include <utility>
 
 #define TEST_MAX 128
@@ -31,6 +32,37 @@
     REQUIRE((cs).size() == 0);                                                 \
     REQUIRE((void *)(cs).head() == (cs).tail());                               \
   } while (0)
+
+// @return   first is replace(all)
+//           second is replace(all)'s count
+//           third is replaceFirst
+//           fourth is replaceFirst's count
+static std::tuple<std::string, int, std::string, int>
+myReplace(const std::string &src, const std::string &from,
+          const std::string &to) {
+  int n1 = 0, n2 = 0, i;
+  std::string s1, s2;
+  bool firstDone = false;
+  for (i = 0; i < src.length() - from.length() + 1;) {
+    if (std::memcmp(src.data() + i, from.data(), from.length()) == 0) {
+      n1++;
+      s1 = s1 + to;
+      i += from.length();
+      if (!firstDone) {
+        s2 = s2 + to;
+        firstDone = true;
+        n2++;
+      } else {
+        s2 = s2 + src[i];
+      }
+    } else {
+      s1 = s1 + src[i];
+      s2 = s2 + src[i];
+      i++;
+    }
+  }
+  return std::make_tuple(s1, n1, s2, n2);
+}
 
 TEST_CASE("Cowstr", "[Cowstr]") {
   fastype::Random::initialize();
@@ -156,31 +188,39 @@ TEST_CASE("Cowstr", "[Cowstr]") {
       for (int i = 0; i < TEST_MAX; i++) {
         std::string s1 = fastype::Random::nextAlphaNumeric(i + 1);
         char cc = fastype::Random::nextAlphaNumericChar();
-        int n = 0;
-        for (int j = 0; j < s1.length(); j++) {
-          if (s1[j] == cc) {
-            n++;
-          }
-        }
         fastype::Cowstr c1(s1);
-        fastype::Cowstr c2 = c1.replace(cc, "");
-        fastype::Cowstr c3 = c1.replace(cc, "world");
-        std::string s2, s3;
-        for (int j = 0; j < s1.length(); j++) {
-          if (s1[j] == cc) {
-            s3 = s3 + "world";
-          } else {
-            s2 = s2 + s1[j];
-            s3 = s3 + s1[j];
-          }
-        }
 
-        REQUIRE(c2.size() == (int)s1.length() - n);
-        REQUIRE(c2.size() == (int)s2.length());
-        REQUIRE(std::memcmp(c2.head(), s2.data(), c2.size()) == 0);
+        std::tuple<std::string, int, std::string, int> replacement1 =
+            myReplace(s1, std::string("") + cc, "");
+        std::string s21 = std::get<0>(replacement1);
+        int n21 = std::get<1>(replacement1);
+        (void)n21;
+        std::string s31 = std::get<2>(replacement1);
+        int n31 = std::get<3>(replacement1);
+        (void)n31;
 
-        REQUIRE(c3.size() == (int)s3.length());
-        REQUIRE(std::memcmp(c3.head(), s3.data(), c3.size()) == 0);
+        fastype::Cowstr c21 = c1.replace(cc, "");
+        fastype::Cowstr c22 = c1.replaceFirst(cc, "");
+        REQUIRE(c21.size() == (int)s21.length());
+        REQUIRE(std::memcmp(c21.head(), s21.data(), c21.size()) == 0);
+        REQUIRE(c22.size() == (int)s31.length());
+        REQUIRE(std::memcmp(c22.head(), s31.data(), c22.size()) == 0);
+
+        std::tuple<std::string, int, std::string, int> replacement2 =
+            myReplace(s1, std::string("") + cc, "world");
+        std::string s22 = std::get<0>(replacement2);
+        int n22 = std::get<1>(replacement2);
+        (void)n22;
+        std::string s32 = std::get<2>(replacement2);
+        int n32 = std::get<3>(replacement2);
+        (void)n32;
+
+        fastype::Cowstr c31 = c1.replace(cc, "world");
+        fastype::Cowstr c32 = c1.replaceFirst(cc, "world");
+        REQUIRE(c31.size() == (int)s22.length());
+        REQUIRE(std::memcmp(c31.head(), s22.data(), c31.size()) == 0);
+        REQUIRE(c32.size() == (int)s32.length());
+        REQUIRE(std::memcmp(c32.head(), s32.data(), c32.size()) == 0);
       }
     }
 
@@ -189,30 +229,24 @@ TEST_CASE("Cowstr", "[Cowstr]") {
         std::string ss1 = fastype::Random::nextAlphaNumeric(i + 1);
         std::string ss2 = fastype::Random::nextAlphaNumeric(
             std::min<int>(ss1.length(), i + 1));
-        int n = 0;
-        std::string s2;
-        int j;
-        for (j = 0; j < ss1.length() - ss2.length() + 1;) {
-          if (std::memcmp(ss1.data() + j, ss2.data(), ss2.length()) == 0) {
-            n++;
-            s2 = s2 + "world";
-            j += ss2.length();
-          } else {
-            s2 = s2 + ss1[j];
-            j++;
-          }
-        }
-        if (j < ss1.length()) {
-          s2 = s2 + ss1.substr(j);
-        }
+
+        std::tuple<std::string, int, std::string, int> replacement =
+            myReplace(ss1, ss2, "world");
+        std::string s2 = std::get<0>(replacement);
+        int n2 = std::get<1>(replacement);
+        (void)n2;
+        std::string s3 = std::get<2>(replacement);
+        int n3 = std::get<3>(replacement);
+        (void)n3;
+
         fastype::Cowstr c1(ss1);
         fastype::Cowstr c2 = c1.replace(ss2, "world");
+        fastype::Cowstr c3 = c1.replaceFirst(ss2, "world");
 
-        if (n > 0) {
-          F_INFO_MSG("you can stop here");
-        }
         REQUIRE(c2.size() == (int)s2.length());
         REQUIRE(std::memcmp(c2.head(), s2.data(), c2.size()) == 0);
+        REQUIRE(c3.size() == (int)s3.length());
+        REQUIRE(std::memcmp(c3.head(), s3.data(), c3.size()) == 0);
       }
     }
   }
