60ba3b7dcdc343a4d4f7a9b7a64a4365004f7e19
linrongbin <rolin@ebay.com>
2019-11-20 10:56:58 +0800

    fix readAll test

--- test/FilerTest.cpp
+++ test/FilerTest.cpp
@@ -9,31 +9,31 @@ TEST_CASE("Filer", "[Filer]") {
   SECTION("readAll") {
     icu::UnicodeString text =
         fastype::Filer::readAll(UNICODE_STRING_SIMPLE("CMakeLists.txt"));
-    REQUIRE(text.length() == 1);
-    REQUIRE(text.tempSubString(0, 30).compare(
+    REQUIRE(text.length() == 221);
+    REQUIRE((int)text.tempSubString(0, 31).compare(
                 UNICODE_STRING_SIMPLE("# Copyright 2019- <fastype.org>")) == 0);
-    REQUIRE(text.tempSubString(80, 104).compare(
+    REQUIRE((int)text.tempSubString(195, 25).compare(
                 UNICODE_STRING_SIMPLE("add_subdirectory(example)")) == 0);
   }
   SECTION("readLines") {
     std::vector<icu::UnicodeString> texts =
         fastype::Filer::readLines(UNICODE_STRING_SIMPLE("CMakeLists.txt"));
     REQUIRE(texts.size() == 8);
-    REQUIRE(texts[0].compare(
+    REQUIRE((int)texts[0].compare(
                 UNICODE_STRING_SIMPLE("# Copyright 2019- <fastype.org>")) == 0);
-    REQUIRE(texts[1].compare(
+    REQUIRE((int)texts[1].compare(
                 UNICODE_STRING_SIMPLE("# Apache License Version 2.0")) == 0);
-    REQUIRE(texts[2].compare(UNICODE_STRING_SIMPLE(
+    REQUIRE((int)texts[2].compare(UNICODE_STRING_SIMPLE(
                 "cmake_minimum_required(VERSION 3.8)")) == 0);
-    REQUIRE(texts[3].compare(UNICODE_STRING_SIMPLE(
+    REQUIRE((int)texts[3].compare(UNICODE_STRING_SIMPLE(
                 "project(fastype-parent VERSION 0.1.0 LANGUAGES CXX)")) == 0);
-    REQUIRE(texts[4].compare(UNICODE_STRING_SIMPLE("add_subdirectory(src)")) ==
-            0);
-    REQUIRE(texts[5].compare(UNICODE_STRING_SIMPLE("add_subdirectory(test)")) ==
-            0);
-    REQUIRE(texts[6].compare(
+    REQUIRE((int)texts[4].compare(
+                UNICODE_STRING_SIMPLE("add_subdirectory(src)")) == 0);
+    REQUIRE((int)texts[5].compare(
+                UNICODE_STRING_SIMPLE("add_subdirectory(test)")) == 0);
+    REQUIRE((int)texts[6].compare(
                 UNICODE_STRING_SIMPLE("add_subdirectory(example)")) == 0);
-    REQUIRE(texts[7].compare(
+    REQUIRE((int)texts[7].compare(
                 UNICODE_STRING_SIMPLE("add_subdirectory(example)")) == 0);
   }
 }
