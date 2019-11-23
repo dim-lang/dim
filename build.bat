b0e36944029528590e802d136390d5761332078e
linrongbin <linrongbin16@gmail.com>
2019-11-23 22:03:21 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -10,35 +10,35 @@ echo [fastype] prepare for %OS%
 echo [fastype] prepare catchorg/Catch2 v2.9.1
 if not exist %ROOT%\test\catch2 (
     cd %ROOT%\test
-    git clone -b 'v2.9.1' --single-branch --depth 1 https://github.com/catchorg/Catch2.git
+    git clone -b v2.9.1 --single-branch --depth 1 https://github.com/catchorg/Catch2.git
     cd %ROOT%
 )
 echo [fastype] prepare catchorg/Catch2 v2.9.1 - done
 echo [fastype] prepare PDCurses PDCurses_3_6
 if not exist %ROOT%\src\PDCurses (
     cd %ROOT%\src
-    git clone -b 'PDCurses_3_6' --single-branch --depth 1 https://github.com/wmcbrine/PDCurses.git
+    git clone -b PDCurses_3_6 --single-branch --depth 1 https://github.com/wmcbrine/PDCurses.git
     cd %ROOT%
 )
 echo [fastype] prepare PDCurses PDCurses_3_6 - done
 echo [fastype] prepare gabime/spdlog v1.3.1
 if not exist %ROOT%\src\spdlog (
     cd %ROOT%\src
-    git clone -b 'v1.3.1' --single-branch --depth 1 https://github.com/gabime/spdlog.git
+    git clone -b v1.3.1 --single-branch --depth 1 https://github.com/gabime/spdlog.git
     cd %ROOT%
 )
 echo [fastype] prepare gabime/spdlog v1.3.1 - done
 echo [fastype] prepare nlohmann/json v3.7.0
 if not exist %ROOT%\src\json (
     cd %ROOT%\src
-    git clone -b 'v3.7.0' --single-branch --depth 1 https://github.com/nlohmann/json.git
+    git clone -b v3.7.0 --single-branch --depth 1 https://github.com/nlohmann/json.git
     cd %ROOT%
 )
 echo [fastype] prepare nlohmann/json v3.7.0 - done
 echo [fastype] prepare boostorg/boost boost-1.70.0
 if not exist src\boost (
     cd %ROOT%\src
-    git clone -b 'boost-1.70.0' --single-branch --depth 1 https://github.com/boostorg/boost.git
+    git clone -b boost-1.70.0 --single-branch --depth 1 https://github.com/boostorg/boost.git
     cd boost
     git submodule update --init
     cd %ROOT%
@@ -53,7 +53,7 @@ echo [fastype] prepare boostorg/boost boost-1.70.0 - done
 echo [fastype] prepare unicode-org/icu release-64-2
 if not exist src\icu (
     cd %ROOT%\src
-    git clone -b 'release-64-2' --single-branch --depth 1 https://github.com/unicode-org/icu.git
+    git clone -b release-64-2 --single-branch --depth 1 https://github.com/unicode-org/icu.git
     cd %ROOT%
 )
 if not exist src\icu\icu4c\lib (
