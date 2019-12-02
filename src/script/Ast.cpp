6495d6dfa1c0636651beab8c134117a8c84067f8
linrongbin <rolin@ebay.com>
2019-12-02 10:17:23 +0800

    remove Ast.cpp

--- src/script/Ast.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2019- <fastype.org>
-// Apache License Version 2.0
-
-#include "script/Ast.h"
-#include "Logging.h"
-#include "exception/NotFoundException.h"
-
-namespace fastype {} // namespace fastype
