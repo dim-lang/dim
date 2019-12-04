959a2a9fb43c6b8a1f9be3113205b737235f4973
linrongbin <rolin@ebay.com>
2019-12-04 14:08:25 +0800

    update macos cmake scripts

--- example/CMakeMacOSX.cmake
+++ example/CMakeMacOSX.cmake
@@ -11,7 +11,6 @@ set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 
-find_package(Curses REQUIRED)
 find_package(Threads REQUIRED)
 
 message(CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR})
@@ -21,8 +20,8 @@ message(CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE})
 message(CMAKE_VERBOSE_MAKEFILE: ${CMAKE_VERBOSE_MAKEFILE})
 
 set(F_INC
-    ${CURSES_INCLUDE_DIRS}
     Threads::Threads
+    /usr/local/opt/ncurses/include
     /usr/local/opt/spdlog/include
     /usr/local/opt/fmt/include
     /usr/local/opt/boost/include
@@ -30,8 +29,9 @@ set(F_INC
     /usr/local/opt/catch2/include
     )
 set(F_LIB
-    ${CURSES_LIBRARIES}
     Threads::Threads
+    ncurses
+    panel
     fmt
     boost_program_options-mt
     boost_system-mt
@@ -40,21 +40,16 @@ set(F_LIB
     icudata
     icui18n
     tcmalloc
-    panel
     )
 set(F_LIB_DIR
     .
-    ${CURSES_LIBRARY_DIRS}
+    /usr/local/opt/ncurses/lib
     /usr/local/opt/spdlog/lib
     /usr/local/opt/fmt/lib
     /usr/local/opt/boost/lib
     /usr/local/opt/icu4c/lib
     /usr/local/opt/gperftools/lib
     )
-
-message(F_INC: ${F_INC})
-message(F_LIB_DIR: ${F_LIB_DIR})
-message(F_LIB: ${F_LIB})
 
 include_directories(${F_INC})
 link_directories(${F_LIB_DIR})
--- test/CMakeMacOSX.cmake
+++ test/CMakeMacOSX.cmake
@@ -12,7 +12,6 @@ set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 
-find_package(Curses REQUIRED)
 find_package(Threads REQUIRED)
 
 message(CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR})
@@ -24,17 +23,19 @@ message(CMAKE_VERBOSE_MAKEFILE: ${CMAKE_VERBOSE_MAKEFILE})
 set(F_INC
     .
     ../src
-    ${CURSES_INCLUDE_DIRS}
     Threads::Threads
+    /usr/local/opt/ncurses/include
     /usr/local/opt/spdlog/include
     /usr/local/opt/fmt/include
     /usr/local/opt/boost/include
     /usr/local/opt/icu4c/include
     /usr/local/opt/catch2/include
+    Catch2/single_include
     )
 set(F_LIB
-    ${CURSES_LIBRARIES}
     Threads::Threads
+    ncurses
+    panel
     fmt
     boost_program_options-mt
     boost_system-mt
@@ -43,23 +44,18 @@ set(F_LIB
     icudata
     icui18n
     tcmalloc
-    panel
     )
 set(F_LIB_DIR
     .
     ..
     ../src
-    ${CURSES_LIBRARY_DIRS}
+    /usr/local/opt/ncurses/lib
     /usr/local/opt/spdlog/lib
     /usr/local/opt/fmt/lib
     /usr/local/opt/boost/lib
     /usr/local/opt/icu4c/lib
     /usr/local/opt/gperftools/lib
     )
-
-message(F_INC: ${F_INC})
-message(F_LIB_DIR: ${F_LIB_DIR})
-message(F_LIB: ${F_LIB})
 
 set(T_SRC
     MainTest.cpp
@@ -79,12 +75,6 @@ set(T_SRC
     )
 
 include_directories(${F_INC})
-include_directories(../src)
-include_directories(../src/spdlog/include)
-include_directories(../src/fmt/include)
-include_directories(../src/boost)
-include_directories(../src/icu/icu4c/source)
-include_directories(Catch2/single_include)
 link_directories(${F_LIB_DIR})
 
 add_executable(fastype-test ${T_SRC})
