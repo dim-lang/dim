5c8cfac131da2eedf6b80897bc457776fefcb282
linrongbin <rolin@ebay.com>
2019-12-08 18:39:05 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -50,14 +50,12 @@ if not exist %ROOT%\src\boost (
     git submodule update --init
     cd %ROOT%
 )
-echo [fastype] prepare boostorg/boost boost-1.70.0 - done
-echo [fastype] prepare unicode-org/icu release-64-2
-if not exist \icu (
-    cd \
-    git clone -b release-64-2 --single-branch --depth 1 https://github.com/unicode-org/icu.git
-    cd %ROOT%
+if not exist %ROOT%\src\boost\stage (
+    cd %ROOT%\src\boost
+    cmd /c .\bootstrap.bat
+    cmd /c .\b2 toolset=msvc-14.1 address-model=64 architecture=x86 link=static threading=multi runtime-link=shared --build-type=complete stage
 )
-echo [fastype] prepare unicode-org/icu release-64-2 - done
+echo [fastype] prepare boostorg/boost boost-1.70.0 - done
 
 echo [fastype] prepare msvc project
 set DEBUG=msvcd
@@ -79,11 +77,9 @@ cd %RELEASE% && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR_PLATFORM=x64 
 echo [fastype] prepare msvc project - done
 
 echo [fastype] 1. please manually build `icu4c` library:
+echo [fastype]      $ cd \
+echo [fastype]      $ git clone -b release-64-2 --single-branch --depth 1 https://github.com/unicode-org/icu.git
 echo [fastype]    open msvc project `\icu\icu4c\source\allinone\allinone.sln`, remove sub projects `common_uwp`, `i18n_uwp`.
 echo [fastype]    build `icu4c` library with option `Debug x64` and `Release x64`, then move `\icu` library to `%ROOT%\src`
 echo [fastype]      $ mv \icu %ROOT%\src
-echo [fastype] 2. please manually build `boost` library:
-echo [fastype]      $ cd %ROOT%\src\boost
-echo [fastype]      $ .\bootstrap.bat
-echo [fastype]      $ .\b2 toolset=msvc-14.1 address-model=64 architecture=x86 link=static threading=multi runtime-link=shared --build-type=complete stage (add `-j8` if you can)
 echo [fastype] 3. please manually build msvc project `%DEBUG%\fastype-parent.sln` with `Debug x64`, `%RELEASE%\fastype-parent.sln` with `Release x64`
