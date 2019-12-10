be8c02397693fb5de196000d01b59898016ac442
linrongbin <rolin@ebay.com>
2019-12-10 17:49:46 +0800

    add support for openSUSE

--- example/icu_ustdio.cpp
+++ example/icu_ustdio.cpp
@@ -12,6 +12,34 @@
 #define SZ1 10
 #define SZ2 50
 
+void writeFile(const icu::UnicodeString &fileName,
+               const icu::UnicodeString &text) {
+  UFILE *fp = u_fopen("CMakeLists.txt", "w", nullptr, "UTF-8");
+  u_fclose(fp);
+}
+
+void getString(const std::string &fileName) {
+  UFILE *fp = u_fopen("CMakeLists.txt", "r", nullptr, "UTF-8");
+  assert(fp != nullptr);
+  UChar buf1[SZ1];
+  UChar *r1 = u_fgets(buf1, SZ1, fp);
+  u_printf(
+      "get line 1, SZ1: %d, buf1: %s, strlen1: %d, buf1[%d]: %c (%d), r1: %s\n",
+      SZ1, buf1, u_strlen(buf1), SZ1 - 1, buf1[SZ1 - 1], (int)buf1[SZ1 - 1],
+      r1);
+  u_fclose(fp);
+
+  fp = u_fopen("CMakeLists.txt", "r", nullptr, "UTF-8");
+  assert(fp != nullptr);
+  UChar buf2[SZ2];
+  UChar *r2 = u_fgets(buf2, SZ2, fp);
+  u_printf(
+      "get line 1, SZ2: %d, buf2: %s, strlen2: %d, buf2[%d]: %c (%d), r2: %s\n",
+      SZ2, buf2, u_strlen(buf2), SZ2 - 1, buf2[SZ2 - 1], (int)buf2[SZ2 - 1],
+      r2);
+  u_fclose(fp);
+}
+
 int main(int argc, char *argv[]) {
   UFILE *fp = u_fopen("CMakeLists.txt", "r", nullptr, "UTF-8");
   assert(fp != nullptr);
