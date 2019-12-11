d769c0b5dc0eee29bd872a24ea9680f9e6626cdd
linrongbin <rolin@ebay.com>
2019-12-11 09:05:33 +0800

    add message

--- src/cmake/linux.cmake
+++ src/cmake/linux.cmake
@@ -15,6 +15,10 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 find_package(Threads REQUIRED)
 find_package(Curses REQUIRED)
 
+message(CURSES_INCLUDE_DIR: ${CURSES_INCLUDE_DIR})
+message(CURSES_LIBRARIES: ${CURSES_LIBRARIES})
+message(CURSES_LIBRARY_DIRS: ${CURSES_LIBRARY_DIRS})
+
 set(F_INC
     .
     Threads::Threads
