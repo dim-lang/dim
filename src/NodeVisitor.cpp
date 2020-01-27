8b50dac183ac2f69e97f2123401b622f560f3da1
linrongbin <rolin@ebay.com>
2020-01-27 17:06:39 +0800

    save for node visitor

--- src/NodeVisitor.cpp
+++ src/NodeVisitor.cpp
@@ -2,12 +2,49 @@
 // Apache License Version 2.0
 
 #include "NodeVisitor.h"
+#include "Log.h"
 
 FunctionCallExpressionVisitor::FunctionCallExpressionVisitor(
     std::shared_ptr<Ast> node)
-    : node_(node) {}
+    : node_(std::static_pointer_cast<AstFunctionCallExpression, Ast>(node)) {}
 
-FunctionCallExpressionVisitor::FunctionCallExpressionVisitor(Ast *node)
-    : node_(std::shared_ptr<Ast>(node)) {}
+std::shared_ptr<Ast> FunctionCallExpressionVisitor::visit() {
+  FINFO("node_:{}", node_ ? node_->toString() : "null");
+  return node_;
+}
 
-std::shared_ptr<Ast> FunctionCallExpressionVisitor::visit() { return node_; }
+UnaryExpressionVisitor::UnaryExpressionVisitor(std::shared_ptr<Ast> node)
+    : node_(std::static_pointer_cast<AstUnaryExpression, Ast>(node)) {}
+
+std::shared_ptr<Ast> UnaryExpressionVisitor::visit() {
+  std::shared_ptr<AstExpression> expr =
+      std::static_pointer_cast<AstExpression, Ast>(node_->expression());
+  switch (node_->token()) {
+  case T_ADD: {
+    switch (expr->type()) {
+    case A_I8_CONSTANT: {
+      break;
+    }
+    default: {
+      goto unary_expression_error;
+    }
+    }
+    break;
+  }
+  case T_SUB: {
+    break;
+  }
+  case T_BIT_NOT: {
+    break;
+  }
+  case T_LOGIC_NOT: {
+    break;
+  }
+  default: {
+    goto unary_expression_error;
+  }
+  }
+
+unary_expression_error:
+  FTHROW("invalid node_:{}", node_ ? node_->toString() : "null");
+}
--- src/NodeVisitor.h
+++ src/NodeVisitor.h
@@ -7,21 +7,26 @@
 
 class NodeVisitor {
 public:
-  NodeVisitor(std::shared_ptr<Ast> node);
-  NodeVisitor(Ast *node);
   virtual ~NodeVisitor() = default;
-
   virtual std::shared_ptr<Ast> visit() = 0;
-
-protected:
-  std::shared_ptr<Ast> node_;
 };
 
 class FunctionCallExpressionVisitor : public NodeVisitor {
 public:
   FunctionCallExpressionVisitor(std::shared_ptr<Ast> node);
-  FunctionCallExpressionVisitor(Ast *node);
   virtual ~FunctionCallExpressionVisitor() = default;
-
   virtual std::shared_ptr<Ast> visit();
+
+private:
+  std::shared_ptr<AstFunctionCallExpression> node_;
+};
+
+class UnaryExpressionVisitor : public NodeVisitor {
+public:
+  UnaryExpressionVisitor(std::shared_ptr<Ast> node);
+  virtual ~UnaryExpressionVisitor() = default;
+  virtual std::shared_ptr<Ast> visit();
+
+private:
+  std::shared_ptr<AstUnaryExpression> node_;
 };
