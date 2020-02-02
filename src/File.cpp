439830de34236e0f214342d47665a45bc5da927f
linrongbin <rolin@ebay.com>
2020-02-02 12:39:32 +0800

    optimize writelineImpl

--- src/File.cpp
+++ src/File.cpp
@@ -5,12 +5,14 @@
 #include "Log.h"
 #include <cstdlib>
 
+#define INIT_LEN 1024
+
 icu::UnicodeString File::read(const icu::UnicodeString &fileName,
                               const char *locale, const char *codepage) {
   UFILE *fp = u_fopen_u(fileName.getBuffer(), "r", locale, codepage);
-  CASSERT(fp != nullptr, "fp {} != nullptr", (void *)fp);
+  CASSERT(fp, "fp is null: {}", (void *)fp);
   UChar *data = nullptr;
-  int len = 1024, n = 0, tot = 0;
+  int len = INIT_LEN, n = 0, tot = 0;
 
   do {
     if (data == nullptr || tot >= len) {
@@ -34,10 +36,10 @@ std::vector<icu::UnicodeString>
 File::readline(const icu::UnicodeString &fileName, const char *locale,
                const char *codepage) {
   UFILE *fp = u_fopen_u(fileName.getBuffer(), "r", locale, codepage);
-  CASSERT(fp != nullptr, "fp {} != nullptr", (void *)fp);
-  int len = 1024;
+  CASSERT(fp, "fp is null: {}", (void *)fp);
+  int len = INIT_LEN;
   UChar *data = (UChar *)malloc(len * sizeof(UChar));
-  CASSERT(data, "realloc error! data {} != nullptr", (void *)data);
+  CASSERT(data, "realloc error! data is null: {}", (void *)data);
   std::vector<icu::UnicodeString> ret;
 
   while (true) {
@@ -57,7 +59,7 @@ File::readline(const icu::UnicodeString &fileName, const char *locale,
       } else {
         len *= 2;
         data = (UChar *)realloc(data, sizeof(UChar) * len);
-        CASSERT(data, "realloc error! data {} != nullptr", (void *)data);
+        CASSERT(data, "realloc error! data is null: {}", (void *)data);
         pos = dataLen;
       }
     }
@@ -70,24 +72,14 @@ end_of_readline:
 }
 
 static int writeImpl(const icu::UnicodeString &fileName,
-                     const icu::UnicodeString &text, const char *perm,
-                     const char *locale, const char *codepage) {
+                     const std::vector<icu::UnicodeString> &texts,
+                     const char *perm, const char *locale,
+                     const char *codepage) {
   UFILE *fp = u_fopen_u(fileName.getBuffer(), perm, locale, codepage);
-  CASSERT(fp, "open file error! fp {} != nullptr", (void *)fp);
-  int n = (int)u_file_write(text.getBuffer(), text.length(), fp);
-  u_fclose(fp);
-  return n;
-}
-
-static int writelineImpl(const icu::UnicodeString &fileName,
-                         const std::vector<icu::UnicodeString> &lines,
-                         const char *perm, const char *locale,
-                         const char *codepage) {
-  UFILE *fp = u_fopen_u(fileName.getBuffer(), perm, locale, codepage);
-  CASSERT(fp, "open file error! fp {} != nullptr", (void *)fp);
+  CASSERT(fp, "open file error! fp is null: {}", (void *)fp);
   int n = 0;
-  for (int i = 0; i < (int)lines.size(); i++) {
-    n += (int)u_file_write(lines[i].getBuffer(), lines[i].length(), fp);
+  for (int i = 0; i < (int)texts.size(); i++) {
+    n += (int)u_file_write(texts[i].getBuffer(), texts[i].length(), fp);
   }
   u_fclose(fp);
   return n;
@@ -96,23 +88,27 @@ static int writelineImpl(const icu::UnicodeString &fileName,
 int File::write(const icu::UnicodeString &fileName,
                 const icu::UnicodeString &text, const char *locale,
                 const char *codepage) {
-  return writeImpl(fileName, text, "w", locale, codepage);
+  std::vector<icu::UnicodeString> ts = {text};
+  return writeImpl(fileName, ts, "w", locale, codepage);
 }
 
 int File::writeline(const icu::UnicodeString &fileName,
                     const std::vector<icu::UnicodeString> &lines,
                     const char *locale, const char *codepage) {
-  return writelineImpl(fileName, lines, "w", locale, codepage);
+  return writeImpl(fileName, lines, "w", locale, codepage);
 }
 
 int File::append(const icu::UnicodeString &fileName,
                  const icu::UnicodeString &text, const char *locale,
                  const char *codepage) {
-  return writeImpl(fileName, text, "a", locale, codepage);
+  std::vector<icu::UnicodeString> ts = {text};
+  return writeImpl(fileName, ts, "a", locale, codepage);
 }
 
 int File::appendline(const icu::UnicodeString &fileName,
                      const std::vector<icu::UnicodeString> &lines,
                      const char *locale, const char *codepage) {
-  return writelineImpl(fileName, lines, "a", locale, codepage);
+  return writeImpl(fileName, lines, "a", locale, codepage);
 }
+
+#undef INIT_LEN
