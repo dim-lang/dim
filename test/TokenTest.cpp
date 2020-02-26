49d79c8ae339b5095cc1efe58eaa3798a519e122
linrongbin <rolin@ebay.com>
2020-02-26 17:49:40 +0800

    add location to flex

--- test/TokenTest.cpp
+++ test/TokenTest.cpp
@@ -9,15 +9,17 @@
 #include <cstdio>
 
 static void go(const char *module) {
+  union YYSTYPE ys;
+  struct YYLTYPE yl;
   REQUIRE(TokenBuffer::pushImport(module) == 1);
   CINFO("go start: {}", module);
   int t;
-  while ((t = yylex()) != 0) {
+  while ((t = yylex(&ys, &yl)) != 0) {
     if (t == T_IDENTIFIER || t == T_I32_CONSTANT || t == T_I8_CONSTANT ||
         t == T_U8_CONSTANT || t == T_I16_CONSTANT || t == T_U16_CONSTANT ||
         t == T_U32_CONSTANT || t == T_I64_CONSTANT || t == T_U64_CONSTANT ||
         t == T_F32_CONSTANT || t == T_F64_CONSTANT || t == T_STRING) {
-      CINFO("token:{}, literal:{}", t, yylval.literal);
+      CINFO("token:{}, literal:{}", t, ys.literal);
     } else {
       CINFO("token:{}", t);
     }
