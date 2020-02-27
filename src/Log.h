1d3d3a9060b98a5b1285d4ae20840a49a6fc8f8b
linrongbin <rolin@ebay.com>
2020-02-27 16:01:40 +0800

    revert CASSERT

--- src/Log.h
+++ src/Log.h
@@ -20,28 +20,25 @@ public:
 #define CTRACE(...)
 #define CDEBUG(...)
 #define CINFO(...)
-#define CASSERT(cond, ...)
 
 #else
 
 #define CTRACE(...) SPDLOG_TRACE(__VA_ARGS__)
 #define CDEBUG(...) SPDLOG_DEBUG(__VA_ARGS__)
 #define CINFO(...) SPDLOG_INFO(__VA_ARGS__)
-#define CASSERT(cond, ...)                                                     \
-  do {                                                                         \
-    if (!(cond)) {                                                             \
-      std::fprintf(stderr,                                                     \
-                   "Assert Fail! %s:%d %s - Condition: %s, Result: %s\n",      \
-                   __FILE__, __LINE__, __FUNCTION__, BOOST_PP_STRINGIZE(cond), \
-                   fmt::format(__VA_ARGS__).c_str());                          \
-    }                                                                          \
-    assert((cond) &&                                                           \
-           (fmt::format("Assert Fail! {}:{} {} - Condition: {}, Result: {}",   \
-                        __FILE__, __LINE__, __FUNCTION__,                      \
-                        BOOST_PP_STRINGIZE(cond), fmt::format(__VA_ARGS__))    \
-                .c_str()));                                                    \
-  } while (0)
 
 #endif // #ifdef NDEBUG
 
 #define CERROR(...) SPDLOG_ERROR(__VA_ARGS__)
+#define CASSERT(cond, ...)                                                     \
+  do {                                                                         \
+    std::fprintf(stderr,                                                       \
+                 "Assert Fail! %s:%d %s - Condition: %s, Result: %s\n",        \
+                 __FILE__, __LINE__, __FUNCTION__, BOOST_PP_STRINGIZE(cond),   \
+                 fmt::format(__VA_ARGS__).c_str());                            \
+    if (!(cond)) {                                                             \
+      throw fmt::format("Assert Fail! {}:{} {} - Condition: {}, Result: {}",   \
+                        __FILE__, __LINE__, __FUNCTION__,                      \
+                        BOOST_PP_STRINGIZE(cond), fmt::format(__VA_ARGS__));   \
+    }                                                                          \
+  } while (0)
