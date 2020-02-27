8c9c9695f2ab595f81ab99d5f69ff3985639e42f
linrongbin <rolin@ebay.com>
2020-02-27 16:04:55 +0800

    update

--- src/Log.h
+++ src/Log.h
@@ -33,12 +33,12 @@ public:
 #define CASSERT(cond, ...)                                                     \
   do {                                                                         \
     if (!(cond)) {                                                             \
-      std::fprintf(stderr,                                                     \
-                   "Assert Fail! %s:%d %s - Condition: %s, Result: %s\n",      \
-                   __FILE__, __LINE__, __FUNCTION__, BOOST_PP_STRINGIZE(cond), \
-                   fmt::format(__VA_ARGS__).c_str());                          \
+      char *fmtmsg = fmt::format(__VA_ARGS__).c_str();                         \
+      std::fprintf(                                                            \
+          stderr, "Assert Fail! %s:%d %s - Condition: %s, Result: %s\n",       \
+          __FILE__, __LINE__, __FUNCTION__, BOOST_PP_STRINGIZE(cond), fmtmsg); \
       throw fmt::format("Assert Fail! {}:{} {} - Condition: {}, Result: {}",   \
                         __FILE__, __LINE__, __FUNCTION__,                      \
-                        BOOST_PP_STRINGIZE(cond), fmt::format(__VA_ARGS__));   \
+                        BOOST_PP_STRINGIZE(cond), fmtmsg);                     \
     }                                                                          \
   } while (0)
