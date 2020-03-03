5c890a8f0e00dcf5c6f8dfb47d6ed6821efe52f8
linrongbin <rolin@ebay.com>
2020-03-03 18:07:07 +0800

    update extend

--- src/container/LinkedHashMap.hpp
+++ src/container/LinkedHashMap.hpp
@@ -226,6 +226,7 @@ LinkedHt<K, V, H, E>::LinkedHt()
 
 template <typename K, typename V, typename H, typename E>
 LinkedHt<K, V, H, E>::LinkedHt(int bucket) : LinkedHt() {
+  CASSERT(bucket > 0, "bucket {} > 0", bucket);
   extend(bucket);
 }
 
@@ -283,7 +284,7 @@ void LinkedHt<K, V, H, E>::release() {
 
 template <typename K, typename V, typename H, typename E>
 void LinkedHt<K, V, H, E>::insert(const std::pair<const K, V> &value) {
-  extend();
+  extend(bucket_ * 2);
   LinkedNode<K, V> *e = new LinkedNode<K, V>(value);
   int b = (int)hasher_(value.first) % bucket_;
   ht_[b].insertHead(e);
@@ -292,7 +293,7 @@ void LinkedHt<K, V, H, E>::insert(const std::pair<const K, V> &value) {
 
 template <typename K, typename V, typename H, typename E>
 int LinkedHt<K, V, H, E>::insertOrAssign(const std::pair<const K, V> &value) {
-  extend();
+  extend(bucket_ * 2);
   LinkedIterator<K, V> position = find(value.first);
   if (position == end()) {
     LinkedNode<K, V> *e = new LinkedNode<K, V>(value);
@@ -363,6 +364,13 @@ bool LinkedHt<K, V, H, E>::isNotNull() {
 
 template <typename K, typename V, typename H, typename E>
 void LinkedHt<K, V, H, E>::extend(int n) {
+  CASSERT(size_ >= 0, "size_ {} >= 0", size_);
+  CASSERT(bucket_ >= 0, "bucket_ {} >= 0", bucket_);
+  CASSERT(n > 0, "n {} > 0", n);
+  if (size_ > 0 && loadFactor() < 4.0) {
+    return;
+  }
+  n = std::max(16, n);
   if (n <= bucket_) {
     return;
   }
