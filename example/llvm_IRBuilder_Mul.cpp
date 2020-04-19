a79012e34a78cfeb4f31e5979998deee08a58871
linrongbin <linrongbin16@outlook.com>
2020-04-19 20:15:26 +0800

    rename IntAdd/IntSub/IntMul/IntDiv to Add/Sub/Mul/Div

--- example/llvm_IRBuilder_Mul.cpp
+++ example/llvm_IRBuilder_Mul.cpp
@@ -23,135 +23,135 @@ int main(void) {
   llvm::LLVMContext context;
   llvm::IRBuilder<> builder(context);
 
-  // case: i8 - i8
-  fmt::print("dump i8 - i8, i8 min: {}, i8 max: {}\n", INT8_MIN, (int)INT8_MAX);
+  // case: i8 * i8
+  fmt::print("dump i8 * i8, i8 min: {}, i8 max: {}\n", INT8_MIN, (int)INT8_MAX);
   {
-    // -7 - -101 = 94
+    // -7 * -10 = 70
     int8_t a = -7;
-    int8_t b = -101;
+    int8_t b = -10;
     llvm::Value *p = llvm::ConstantInt::get(context, llvm::APInt(8, a, true));
     llvm::Value *q = llvm::ConstantInt::get(context, llvm::APInt(8, b, true));
-    llvm::Value *r = builder.CreateSub(p, q, getOpTmp(Op::Sub));
+    llvm::Value *r = builder.CreateMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("{} {} {} = ", a, getOpName(Op::Sub), b);
+    fmt::print("{} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
   {
-    // -128 - 1 = ? overflow
-    int8_t a = -128;
-    int8_t b = 1;
+    // -12 * 50 = ? overflow
+    int8_t a = -12;
+    int8_t b = 50;
     llvm::Value *p = llvm::ConstantInt::get(context, llvm::APInt(8, a, true));
     llvm::Value *q = llvm::ConstantInt::get(context, llvm::APInt(8, b, true));
-    llvm::Value *r = builder.CreateSub(p, q, getOpTmp(Op::Sub));
+    llvm::Value *r = builder.CreateMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Sub), b);
+    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
   {
-    // 1 - -127 = ? overflow
-    int8_t a = 1;
+    // 13 * -127 = ? overflow
+    int8_t a = 13;
     int8_t b = -127;
     llvm::Value *p = llvm::ConstantInt::get(context, llvm::APInt(8, a, true));
     llvm::Value *q = llvm::ConstantInt::get(context, llvm::APInt(8, b, true));
-    llvm::Value *r = builder.CreateSub(p, q, getOpTmp(Op::Sub));
+    llvm::Value *r = builder.CreateMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Sub), b);
+    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
 
-  // case: i8 - u8
-  fmt::print("\ndump i8 - u8, i8 min: {}, i8 max: {}, u8 min: {}, u8 max: {}\n",
+  // case: i8 * u8
+  fmt::print("\ndump i8 * u8, i8 min: {}, i8 max: {}, u8 min: {}, u8 max: {}\n",
              INT8_MIN, (int)INT8_MAX, 0U, UINT8_MAX);
   {
-    // 7 - 102 = -95
+    // 7 * 12 = 84
     int8_t a = 7;
-    uint8_t b = 102;
+    uint8_t b = 12;
     llvm::Value *p = llvm::ConstantInt::get(context, llvm::APInt(8, a, true));
     llvm::Value *q = llvm::ConstantInt::get(context, llvm::APInt(8, b, false));
-    llvm::Value *r = builder.CreateSub(p, q, getOpTmp(Op::Sub));
+    llvm::Value *r = builder.CreateMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("{} {} {} = ", a, getOpName(Op::Sub), b);
+    fmt::print("{} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
   {
-    // -10 - 55 = -65
-    int8_t a = -10;
-    uint8_t b = 55;
+    // -15 * 5 = -75
+    int8_t a = -15;
+    uint8_t b = 5;
     llvm::Value *p = llvm::ConstantInt::get(context, llvm::APInt(8, a, true));
     llvm::Value *q = llvm::ConstantInt::get(context, llvm::APInt(8, b, false));
-    llvm::Value *r = builder.CreateSub(p, q, getOpTmp(Op::Sub));
+    llvm::Value *r = builder.CreateMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("{} {} {} = ", a, getOpName(Op::Sub), b);
+    fmt::print("{} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
   {
-    // -1 - 128 = ? overflow
+    // -1 * 255 = ? overflow
     int8_t a = -1;
-    uint8_t b = 128;
+    uint8_t b = 255;
     llvm::Value *p = llvm::ConstantInt::get(context, llvm::APInt(8, a, true));
     llvm::Value *q = llvm::ConstantInt::get(context, llvm::APInt(8, b, false));
-    llvm::Value *r = builder.CreateSub(p, q, getOpTmp(Op::Sub));
+    llvm::Value *r = builder.CreateMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Sub), b);
+    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
 
-  // case: u8 - u8
-  fmt::print("\ndump u8 - u8, u8 min: {}, u8 max: {}\n", 0U, UINT8_MAX);
+  // case: u8 * u8
+  fmt::print("\ndump u8 * u8, u8 min: {}, u8 max: {}\n", 0U, UINT8_MAX);
   {
-    // 37 - 10 = 27
-    uint8_t a = 37;
-    uint8_t b = 10;
+    // 7 * 13 = 91
+    uint8_t a = 7;
+    uint8_t b = 13;
     llvm::Value *p = llvm::ConstantInt::get(context, llvm::APInt(8, a, false));
     llvm::Value *q = llvm::ConstantInt::get(context, llvm::APInt(8, b, false));
-    llvm::Value *r = builder.CreateSub(p, q, getOpTmp(Op::Sub));
+    llvm::Value *r = builder.CreateMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("{} {} {} = ", a, getOpName(Op::Sub), b);
+    fmt::print("{} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
   {
-    // 1 - 2 = ? overflow
-    uint8_t a = 1;
-    uint8_t b = 2;
+    // 10 * 29 = 290 overflow
+    uint8_t a = 10;
+    uint8_t b = 29;
     llvm::Value *p = llvm::ConstantInt::get(context, llvm::APInt(8, a, false));
     llvm::Value *q = llvm::ConstantInt::get(context, llvm::APInt(8, b, false));
-    llvm::Value *r = builder.CreateSub(p, q, getOpTmp(Op::Sub));
+    llvm::Value *r = builder.CreateMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Sub), b);
+    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
 
-  // case: f32 - f32
-  fmt::print("\ndump f32 - f32, f32 min: {}, f32 max: {}\n", FLT_MIN, FLT_MAX);
+  // case: f32 * f32
+  fmt::print("\ndump f32 * f32, f32 min: {}, f32 max: {}\n", FLT_MIN, FLT_MAX);
   {
-    // 31.2 - 73.1 = -41.9
+    // 31.2 * 73.1 = 2280.72
     float a = 31.2;
     float b = 73.1;
     llvm::Value *p = llvm::ConstantFP::get(context, llvm::APFloat(a));
     llvm::Value *q = llvm::ConstantFP::get(context, llvm::APFloat(b));
-    llvm::Value *r = builder.CreateFAdd(p, q, getOpTmp(Op::Add));
+    llvm::Value *r = builder.CreateFMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("{} {} {} = ", a, getOpName(Op::Add), b);
+    fmt::print("{} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
   {
-    // FLT_MAX-1.1 - -20.1 = ? overflow
+    // FLT_MAX-1.1 * 2.1 = ? overflow
     float a = FLT_MAX - 1.1;
-    float b = -20.1;
+    float b = 2.1;
     llvm::Value *p = llvm::ConstantFP::get(context, llvm::APFloat(a));
     llvm::Value *q = llvm::ConstantFP::get(context, llvm::APFloat(b));
-    llvm::Value *r = builder.CreateFAdd(p, q, getOpTmp(Op::Add));
+    llvm::Value *r = builder.CreateFMul(p, q, getOpTmp(Op::Mul));
     llvm::raw_os_ostream os(std::cout);
-    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Add), b);
+    fmt::print("overflow {} {} {} = ", a, getOpName(Op::Mul), b);
     r->print(os, true);
     fmt::print("\n");
   }
