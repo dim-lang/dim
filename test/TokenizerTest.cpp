7c65a86188f7c645a16d2dbc2d8db9360e76eb6c
linrongbin <rolin@ebay.com>
2020-08-29 05:26:19 +0800

    optimize testing and YYDEBUG

--- test/TokenizerTest.cpp
+++ test/TokenizerTest.cpp
@@ -4,10 +4,12 @@
 #include "Log.h"
 #include "Scanner.h"
 #include "catch2/catch.hpp"
+#include <vector>
 
 static void tokenize(const char *fileName) {
   Scanner scanner(fileName);
   LOG_INFO("tokenizer start: {}", fileName);
+  std::vector<std::string> tokenList;
   while (true) {
     yy::parser::symbol_type t(scanner.tokenize());
     if (t.kind() == yy::parser::symbol_kind::S_YYEOF) {
@@ -18,13 +20,18 @@ static void tokenize(const char *fileName) {
         t.kind() == yy::parser::symbol_kind::S_T_FLOAT_LITERAL ||
         t.kind() == yy::parser::symbol_kind::S_T_CHARACTER_LITERAL ||
         t.kind() == yy::parser::symbol_kind::S_T_VAR_ID) {
-      LOG_INFO("token name:{} string value:{}", t.name(),
-               t.value.as<std::string>());
+      LOG_INFO("token:{} tag:{}", t.value.as<std::string>(), t.name());
+      tokenList.push_back(t.value.as<std::string>());
     } else {
-      LOG_INFO("token name:{} int value:{}", t.name(), t.value.as<int>());
+      LOG_INFO("token:{} tag:{}", t.name(), t.value.as<int>());
+      tokenList.push_back(t.name());
     }
   }
-  // we don't need pop file manually here
+  std::stringstream ss;
+  for (int i = 0; i < (int)tokenList.size(); i++) {
+    ss << tokenList[i] << " ";
+  }
+  LOG_INFO("tokenizer end: {}", ss.str());
 }
 
 TEST_CASE("Token", "[Token]") {
